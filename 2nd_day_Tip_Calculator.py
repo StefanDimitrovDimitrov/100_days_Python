47ae2a517e71dd341d4fcff208830ed35325aeae
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2020-12-27 22:54:17 +0200

    Initial_Commit

---
+++ 2nd_day_Tip_Calculator.py
@@ -0,0 +1,10 @@
+print("Welcome to the tip calculator!")
+total_bill = float(input("What was tthe total bill? $"))
+per_tip = int(input("What percentage tip would you like to give? "))
+friends = int(input("How many people to split the bill? "))
+
+bill_plus_tip = total_bill + total_bill * per_tip/100
+
+bill = bill_plus_tip/friends
+
+print(f"Each person should pay: ${bill:.2f} ")
