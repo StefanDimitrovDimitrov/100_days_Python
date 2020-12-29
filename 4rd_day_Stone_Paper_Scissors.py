5606da17bfb89a644f5d035584fdb816f65a58bd
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2020-12-29 09:48:45 +0200

    fixed_the_logic

--- 4rd_day_Stone_Paper_Scissors.py
+++ 4rd_day_Stone_Paper_Scissors.py
@@ -30,24 +30,25 @@ scissors = '''
 list_rps = [rock, paper, scissors]
 list_num = [0, 1, 2]
 
-user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
-print("Your chose:")
-user_choice = list_rps[user_input - 1]
-user_index = user_input - 1
-print(user_choice)
-
-
-random.shuffle(list_num)
-
-computer_choice = list_rps[list_num[0]]
-computer_choice_index = list_num[0]
-print("Computer chose:")
-print(computer_choice)
-
-if user_index == computer_choice_index:
-    print("Draw")
-elif user_index > computer_choice_index:
-    print("You Won")
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number, you lose!")
 else:
-    print("You lose")
+    print("Your chose:\n" + list_rps[user_choice])
+
+    random.shuffle(list_num)
+
+    computer_choice = list_num[0]
+    print("Computer chose:\n" + list_rps[list_num[0]])
+
+    if user_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif computer_choice == 0 and user_choice == 2:
+        print("You lose")
+    elif computer_choice > user_choice:
+        print("You lose")
+    elif user_choice > computer_choice:
+        print("You win")
+    elif computer_choice == user_choice:
+        print("It's a draw")
