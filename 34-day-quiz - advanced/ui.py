d12dfd7d7b14fcb1b4ac57c92bb1eee6cbb8d66a
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-04-02 16:02:09 +0300

    34- quiz Tkinter Game

---
+++ 34-day-quiz - advanced/ui.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+THEME_COLOR = "#375362"
+
+
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizz")
+
+        self.window.mainloop()
