7b6528c3bd83de8c01e4990ba227a8422c730c0d
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-04-23 12:23:32 +0300

    find element/s by class id, xpath

--- 48 - day - Selenium/main.py
+++ 48 - day - Selenium/main.py
@@ -2,7 +2,22 @@ from selenium import webdriver
 chrome_driver_path = "C:\Forest\GoogleDriver\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get("https://www.amazon.com")
+# driver.get("https://www.amazon.com")
+# price = driver.find_element_by_id("priceblock_ourprice")
+# print(price.text)
 
-driver.close() # close the current tab
+driver.get("https://www.python.org/")
+search_bar= driver.find_element_by_name("q")
+print(search_bar.get_attribute("placeholder"))
+
+logo = driver.find_element_by_class_name("python-logo")
+
+documentation_link = driver.find_element_by_css_selector(".documentation-widget a")
+print(documentation_link.text)
+
+python_wiki = driver.find_element_by_xpath('//*[@id="container"]/li[4]/ul/li[10]/a')
+print(python_wiki.text)
+
+
+# driver.close() # close the current tab
 driver.quit()  # exit the website or web app
