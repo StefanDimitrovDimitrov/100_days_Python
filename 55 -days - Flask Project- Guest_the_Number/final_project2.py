b7487f9aa80a667abb5a0967f0f240dc6318bd64
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-04-28 18:16:58 +0300

    update

--- 55 -days - Flask Project- Guest_the_Number/final_project2.py
+++ 55 -days - Flask Project- Guest_the_Number/final_project2.py
@@ -3,59 +3,49 @@ from flask import Flask
 
 app = Flask(__name__)
 
-target_number = random.randint(0, 10)
-print(target_number)
+target_number = random.randint(0, 20)
 
-list_gift_low = [
-    "https://media.giphy.com/media/XJLEXP9xEJRevqXxnR/giphy.gif",
-    "https://media.giphy.com/media/6MSPYu86rQHzLEdZzQ/giphy.gif",
-    "https://media.giphy.com/media/Zdg7tS9JPHO1cytujy/giphy.gif",
-    "https://media.giphy.com/media/ffDDmEIUS15ra/giphy.gif",
-]
 
-list_gift_high = [
-    "https://media.giphy.com/media/3o7abAHdYvZdBNnGZq/giphy.gif",
-    "https://media.giphy.com/media/26ufplp8yheSKUE00/giphy.gif",
-    "https://media.giphy.com/media/l41lIioP4RFRmIVB6/giphy.gif",
-    "hhttps://media.giphy.com/media/mufPePT1wysiA/giphy.gif",
-]
+def low_link():
+    list_gift_low = [
+        "https://media.giphy.com/media/XJLEXP9xEJRevqXxnR/giphy.gif",
+        "https://media.giphy.com/media/6MSPYu86rQHzLEdZzQ/giphy.gif",
+        "https://media.giphy.com/media/Zdg7tS9JPHO1cytujy/giphy.gif",
+        "https://media.giphy.com/media/ffDDmEIUS15ra/giphy.gif",
+    ]
+    return random.choice(list_gift_low)
+
+
+def high_link():
+    list_gift_high = [
+        "https://media.giphy.com/media/3o7abAHdYvZdBNnGZq/giphy.gif",
+        "https://media.giphy.com/media/26ufplp8yheSKUE00/giphy.gif",
+        "https://media.giphy.com/media/l41lIioP4RFRmIVB6/giphy.gif",
+        "https://media.giphy.com/media/mufPePT1wysiA/giphy.gif",
+    ]
+
+    return random.choice(list_gift_high)
+
 
 won_url = "https://media.giphy.com/media/1dMNqVx9Kb12EBjFrc/giphy.gif"
 
 list_color = ["blue", "red", "pink", "purple", "orange", "green", "black"]
 
-dict_result = {
-    'win': f'<h1 style="color:{random.choice(list_color)}">You found me!</h1><img src={won_url} width=500>',
-    'low': f'<h1 style="color:{random.choice(list_color)}">Too low. Try again!</h1><img src={random.choice(list_gift_low)} width=500>',
-    'high': f'<h1 style="color:{random.choice(list_color)}">Too high. Try again!</h1><img src={random.choice(list_gift_high)} width=500>',
-}
-#
-# def final_result(result):
-#     if result == 'win':
-#         return f'<h1 style="color:{random.choice(list_color)}">You found me!</h1><img src={won_url} width=500>'
-#     elif result == 'low':
-#         return f'<h1 style="color:{random.choice(list_color)}">Too low. Try again!</h1><img src={random.choice(list_gift_low)} width=500>'
-#     elif result == 'high':
-#         return f'<h1 style="color:{random.choice(list_color)}">Too high. Try again!</h1><img src={random.choice(list_gift_high)} width=500>'
-
 
 @app.route('/')
 def guess_the_number():
-    return '<h1>"Guess a number between 0 and 9"</h1>' \
+    return '<h1>"Guess a number between 0 and 20"</h1>' \
            '<img src="https://media.giphy.com/media/l378khQxt68syiWJy/giphy.gif" width=200>'
 
 
 @app.route('/<int:number>')
 def number(number):
-    result = ''
     if number == target_number:
-        result = 'win'
+        return f'<h1 style="color:{random.choice(list_color)}">You found me!</h1><img src={won_url} width=500>'
     elif number < target_number:
-        result = 'low'
+        return f'<h1 style="color:{random.choice(list_color)}">Too low. Try again!</h1><img src={low_link()} width=500>'
     elif number > target_number:
-        result = 'high'
-
-    return dict_result[result]
+        return f'<h1 style="color:{random.choice(list_color)}">Too high. Try again!</h1><img src={high_link()} width=500>'
 
 
 if __name__ == "__main__":
