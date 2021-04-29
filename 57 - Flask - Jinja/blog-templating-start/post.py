f2866798618948ebf12baa70272ae75b3819bf90
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-04-29 09:59:58 +0300

    bootstrap navBar

--- 57 - Flask - Jinja/blog-templating-start/post.py
+++ 57 - Flask - Jinja/blog-templating-start/post.py
@@ -5,7 +5,7 @@ import requests
 class Post:
 
     def __init__(self):
-        self.url_Qs = 'https://api.npoint.io/f00bece59a4fa785a3ae'
+        self.url_Qs = 'https://api.npoint.io/57ad038392ed0f077f03'
 
     def get_q(self):
         response = requests.get(self.url_Qs)
